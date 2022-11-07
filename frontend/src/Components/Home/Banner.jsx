d10b251ed822b50ba299c004d2844c51f3a409f7
Hitesh Tarwani <hiteshtarwani4@gmail.com>
2022-11-07 07:01:44 -0800

    Update Banner.jsx

--- frontend/src/Components/Home/Banner.jsx
+++ frontend/src/Components/Home/Banner.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import styles from "./styles/Banner.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Banner = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.container}>
-      <div className={styles.part}>
+      <div className={styles.part} onClick={() => navigate("/categories")}>
         <img
           src="https://images.bewakoof.com/web/Gender-Picker-Banner-desktop-header-v1NEW.jpg"
           alt=""
         />
       </div>
       <div className={styles.flex}>
-        <div>
+        <div onClick={() => navigate("/mens")}>
           <img
             src="https://images.bewakoof.com/web/Gender-Picker-Banner-desktop-mendesktopNEW-1651231162.jpg"
             alt=""
           />
         </div>
-        <div>
+        <div onClick={() => navigate("/womens")}>
           <img
             src="https://images.bewakoof.com/web/Gender-Picker-Banner-desktop-Womendesktop-1651231163NEW.jpg"
             alt=""
           />
         </div>
       </div>
-      <div className={styles.part}>
+      <div className={styles.part} onClick={() => navigate("/categories")}>
         <img
           src="https://images.bewakoof.com/web/Gender-Picker-Banner-desktop-footer-v1NEW.jpg"
           alt=""
